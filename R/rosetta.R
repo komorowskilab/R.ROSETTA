89aaa29d3996e15360ac690c5939662b70840737
Mateusz Garbulowski <mateuszgarbulowski@gmail.com>
2018-01-17 15:50:35 +0100

    Update rosetta.R

--- R/rosetta.R
+++ R/rosetta.R
@@ -190,7 +190,7 @@ rosetta <- function(df,
                    paste0(dirList2,"/",rosFileName)
       )
     }
-    cmdSys<-try(system(command=comm, show.output.on.console = FALSE, invisible=FALSE, ignore.stderr = TRUE))
+    capture.output(system(command=comm, show.output.on.console = FALSE))
   }
   
   # prepare all results
