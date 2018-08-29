62828d2c007cd42c1b262808efb27902fa1a2b06
Mateusz Garbulowski <mateuszgarbulowski@gmail.com>
2018-08-29 11:37:12 +0200

    Update rosResults.R

--- R/rosResults.R
+++ R/rosResults.R
@@ -7,8 +7,44 @@ rosResults<-function(path, roc){
   }else{
   stats=statsTab[((dim(statsTab)[1]-4):dim(statsTab)[1]),]
   }
+  
+  
   stats2=as.data.frame(as.matrix(stats)[,c(1,3)])
-  colnames(stats2)=c("Measure","Value")
+  colnames(stats2)<-c("Measure","Value")
+  
+  ##MCC
+  cts<-statsTab[which(statsTab$V2=="|"),]
+  #cts<-cts[-which(cts$V1=="Undefined"),]
+  noc<-unique(as.numeric(as.character(cts$V1)))
+  MCC=c()
+
+for(i in seq(1, dim(cts)[1], by = length(noc))){
+if(noc == 2){
+  
+  TP=as.numeric(as.character(cts$V3[i]))
+  FN=as.numeric(as.character(cts$V3[i+1]))
+  FP=as.numeric(as.character(cts$V4[i]))
+  TN=as.numeric(as.character(cts$V4[i+1]))
+  MCC[i]<-((TP*TN)-(FP*FN))/sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
+    
+}else{
+  
+  MCC=NaN
+  ##in progress##
+  #TP=cts$V3[i]
+  #FN=cts$V3[i+1]
+  #FP=cts$V4[i]
+  #TN=cts$V4[i+1]
+  
+}
+  statsMCC=data.frame("MCC.mean",mean(MCC))
+  colnames(statsMCC)<-c("Measure","Value")
+}
+  
+  stats2=rbind(stats2,statsMCC)
+  
+  
+  
   return(stats2)
 
 }
