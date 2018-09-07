c98af4054bc77052d50acb3830da72770993dcc2
Mateusz Garbulowski <mateuszgarbulowski@gmail.com>
2018-09-07 13:45:37 +0200

    Update dfToCsv.R

--- R/dfToCsv.R
+++ R/dfToCsv.R
@@ -24,13 +24,17 @@ dfToCsv <- function(df, fname="data", tempDirNam, disc, firstRow=T)
   
   fRow=data.frame(t(fRow))
   colnames(fRow)<-colnames(df)
-  OUTPUT_DT_TRAIN=rbind(df,fRow)
+  dfc<-data.frame(trimws(as.matrix(format(df, digits=6))))
+  OUTPUT_DT_TRAIN=rbind(dfc,fRow)
   OUTPUT_DT_TRAIN=rbind(OUTPUT_DT_TRAIN[dim(OUTPUT_DT_TRAIN)[1],],OUTPUT_DT_TRAIN[-dim(OUTPUT_DT_TRAIN)[1],])
-
+  
+  write.csv2(OUTPUT_DT_TRAIN,file=paste0(tempDirNam,"/data/",fname,".csv"),quote = F, row.names = F)
+  
+  
   #write.table(fRow,file=paste0(tempDirNam,"/data/",fname,".csv"),sep="\t",quote = F, row.names = F)
   #write.table(format(df, digits=6),file=paste0(tempDirNam,"/data/",fname,".csv"),sep="\t",quote = F,col.names = F, row.names = F, append = TRUE)
-  write.table(fRow,file=paste0(tempDirNam,"/data/",fname,".csv"),sep=",",quote = F, row.names = F)
-  dfc<-trimws(as.matrix(format(df, digits=6)))
-  write.table(dfc,file=paste0(tempDirNam,"/data/",fname,".csv"),sep=",",quote = F,col.names = F, row.names = F, append = TRUE)
+  #write.table(fRow,file=paste0(tempDirNam,"/data/",fname,".csv"),sep=",",quote = F, row.names = F)
+  
+  #write.table(dfc,file=paste0(tempDirNam,"/data/",fname,".csv"),sep=",",quote = F,col.names = F, row.names = F, append = TRUE)
   
   }
