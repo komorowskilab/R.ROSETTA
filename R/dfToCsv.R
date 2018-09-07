7a2bf6253c5a809d501fd0482a4e770b38c35d43
Mateusz Garbulowski <mateuszgarbulowski@gmail.com>
2018-09-07 13:20:14 +0200

    Update dfToCsv.R

--- R/dfToCsv.R
+++ R/dfToCsv.R
@@ -27,7 +27,7 @@ dfToCsv <- function(df, fname="data", tempDirNam, disc, firstRow=T)
   OUTPUT_DT_TRAIN=rbind(df,fRow)
   OUTPUT_DT_TRAIN=rbind(OUTPUT_DT_TRAIN[dim(OUTPUT_DT_TRAIN)[1],],OUTPUT_DT_TRAIN[-dim(OUTPUT_DT_TRAIN)[1],])
 
-  write.table(fRow,file=paste0(tempDirNam,"/data/",fname,".csv"),sep="\t",quote = F, row.names = F)
-  write.table(format(df, digits=6),file=paste0(tempDirNam,"/data/",fname,".csv"),sep="\t",quote = F,col.names = F, row.names = F, append = TRUE)
+  write.table(fRow,file=paste0(tempDirNam,"/data/",fname,".csv"),sep=",",quote = F, row.names = F)
+  write.table(format(df, digits=6),file=paste0(tempDirNam,"/data/",fname,".csv"),sep=",",quote = F,col.names = F, row.names = F, append = TRUE)
   
   }
