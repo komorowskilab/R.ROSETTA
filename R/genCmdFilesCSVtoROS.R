e74f36de2e5540e9ec3e0ea55e20c61f505d371a
Mateusz Garbulowski <mateuszgarbulowski@gmail.com>
2018-09-19 14:28:34 +0200

    Update genCmdFilesCSVtoROS.R

--- R/genCmdFilesCSVtoROS.R
+++ R/genCmdFilesCSVtoROS.R
@@ -16,13 +16,13 @@ genCmdFilesCSVtoROS <- function (dir_file, file_name){
   lista_out[6]=paste("{FILENAME=",dir_file,"/",file_name, ".ros}", sep="")
   write.table(lista_out,file=paste(dir_file,"/",fn, sep=""), quote=F, col.names = F, row.names = F)
   }else{
+  lista_out[1]="StructureCreator"
   lista_out[2]="{OUTPUT = DecisionTable}"
   lista_out[3]="MyDecisionTableImporter"
   lista_out[4]=paste("{FILENAME=",dir_file,"\\",file_name, ".csv}", sep="")
   lista_out[5]="Saver"
   lista_out[6]=paste("{FILENAME=",dir_file,"\\",file_name, ".ros}", sep="")
   write.table(lista_out,file=paste(dir_file,"\\",fn, sep=""), quote=F, col.names = F, row.names = F)
-  
   }
     
   return(fn)
