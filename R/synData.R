b67ef3bca32f08610a66e67fc274daeccdccfdd3
mategarb <mateuszgarbulowski@gmail.com>
2019-12-18 13:28:24 +0100

    synthetic data distributions

--- R/synData.R
+++ R/synData.R
@@ -1,4 +1,4 @@
-synData <- function(nFeatures=c(10,5,3,2,2), rf=c(0.2,0.2,0.2,0.2,0.2), rd=c(0.4,0.5,0.6,0.7,0.8), nObjects=120, nOutcome=2, unbalanced=F, pUnbalancedClass=0.8, discrete=F, levels=4, labels=c("A","C","G","T"), seed=1){
+synData <- function(nFeatures=c(10,5,3,2,2), rf=c(0.2,0.2,0.2,0.2,0.2), rd=c(0.4,0.5,0.6,0.7,0.8), nObjects=120, nOutcome=2, distribution="uniform", unbalanced=F, pUnbalancedClass=0.8, discrete=F, levels=4, labels=c("A","C","G","T"), binProb=0.5, seed=1){
   
   set.seed(seed)
   
@@ -21,7 +21,7 @@ synData <- function(nFeatures=c(10,5,3,2,2), rf=c(0.2,0.2,0.2,0.2,0.2), rd=c(0.4
     for(i in 1:crn){
       class_rest <- c(class_rest,rep(i+1, rpcrn))
     }
-
+    
   }else{
     class_1_on <- round(p*nobs)
     class_rest_on <- nobs-class_1_on
@@ -47,40 +47,65 @@ synData <- function(nFeatures=c(10,5,3,2,2), rf=c(0.2,0.2,0.2,0.2,0.2), rd=c(0.4
     Mr <- as.matrix(Rs) + diag(props[i]+1)
     U  <-  suppressWarnings(t(chol(Mr, pivot = TRUE)))
     
-    if(discrete){
-      rand_dt <- matrix(runif(props[i]*nobs,1,levels), nrow=props[i], ncol=nobs)
-    }else{
-      rand_dt <- matrix(rnorm(props[i]*nobs,0,1), nrow=props[i], ncol=nobs)
-    }
-   
-    rand_dt <- rbind(decision, rand_dt)
-    X <- U %*% rand_dt
-    
-    
     rescale <- function(x) (x-min(x))/(max(x) - min(x)) * (levels-1)+1
     
     if(discrete){
-      if(props[i] == 1){
-        newX <- as.data.frame(apply(X[-1,], 1, function(x) rescale(x)))
-      }else{
-        newX <- as.data.frame(apply(X[-1,], 1, function(x) rescale(x)))
+      
+      if(distribution == "uniform"){
+        rand_dt <- matrix(runif(props[i]*nobs, 0, levels-1), nrow=props[i], ncol=nobs)
+        rand_dt <- rbind(decision, rand_dt)
+        X <- U %*% rand_dt
+        
+        if(props[i] == 1){
+          newX <- as.data.frame(apply(X[-1,], 1, function(x) rescale(x)))
+        }else{
+          newX <- as.data.frame(apply(X[-1,], 1, function(x) rescale(x)))
+        }
+        
       }
+      if(distribution == "binomial"){
+        rand_dt <- matrix(rbinom(props[i]*nobs, levels-1, binProb), nrow=props[i], ncol=nobs)
+        rand_dt <- rbind(decision, rand_dt)
+        X <- U %*% rand_dt
+        
+        if(props[i] == 1){
+          newX <- as.data.frame(apply(X[-1,], 1, function(x) rescale(x)))
+        }else{
+          newX <- as.data.frame(apply(X[-1,], 1, function(x) rescale(x)))
+        }
+      }
+      
     }else{
-      if(props[i] == 1){
-        newX <- as.data.frame(X[-1,])
-      }else{
-        newX <- as.data.frame(t(X[-1,]))  
+      if(distribution == "uniform"){
+        rand_dt <- matrix(runif(props[i]*nobs,-1,1), nrow=props[i], ncol=nobs)
+        rand_dt <- rbind(decision, rand_dt)
+        X <- U %*% rand_dt
+        
+        if(props[i] == 1){
+          newX <- as.data.frame(X[-1,])
+        }else{
+          newX <- as.data.frame(t(X[-1,]))  
+        }
+      }
+      if(distribution == "normal"){
+        rand_dt <- matrix(rnorm(props[i]*nobs,0,1), nrow=props[i], ncol=nobs)
+        rand_dt <- rbind(decision, rand_dt)
+        X <- U %*% rand_dt
+        
+        if(props[i] == 1){
+          newX <- as.data.frame(X[-1,])
+        }else{
+          newX <- as.data.frame(t(X[-1,]))  
+        }
       }
     }
-
-    colnames(newX) <- paste0("f",i,".",1:props[i],"_rf",rf[i],"_rd",rd[i])
-   
     
+    colnames(newX) <- paste0("f",i,".",1:props[i],"_rf",rf[i],"_rd",rd[i])
     adf <- data.frame(adf, newX)
     
   }
-
-
+  
+  
   if(discrete){
     decision <- as.character(paste0("D",decision))
     adf2 <- as.data.frame(cbind(sapply(round(adf[,-1]), function(i) labels[i]), decision))
