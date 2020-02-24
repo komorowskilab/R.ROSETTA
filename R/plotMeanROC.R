43eb35515328ebee2a22dab43141e3ca04363758
mategarb <mateuszgarbulowski@gmail.com>
2020-02-24 15:26:11 +0100

    colours

--- R/plotMeanROC.R
+++ R/plotMeanROC.R
@@ -5,9 +5,9 @@ plotMeanROC <- function(out){
   Sens <- rowMeans(unstack(ROC.stats, form = Sensitivity ~ CVNumber))
   
   ## plotting
-  plot(OMSpec, Sens, type = "l", lwd=3, col="paleturquoise4", main="mean ROC curve", xlab="1 - Specificity (FPR)", ylab="Sensitivity (TPR)", axes=F)
-  polygon(c(1,OMSpec), c(0,Sens), lwd=0.01, col="paleturquoise")
-  text(0.75, 0.1, col="paleturquoise4", paste0("mean AUC = ",round(out$quality$ROC.AUC.MEAN, digits = 2)),cex = 1.5)
+  plot(OMSpec, Sens, type = "l", lwd=3, col="orangered3", xlab="1 - specificity (FPR)", ylab="sensitivity (TPR)", axes=F, cex.lab=1.5)
+  polygon(c(1,OMSpec), c(0,Sens), lwd=0.01, col="snow2")
+  text(0.75, 0.1, col="orangered3", paste0("mean AUC = ",round(out$quality$ROC.AUC.MEAN, digits = 2)),cex = 1.5)
   axis(side=1, at=seq(0,1,0.1))
   axis(side=2, at=seq(0,1,0.1))
   
