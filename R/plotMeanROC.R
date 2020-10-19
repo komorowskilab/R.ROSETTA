a1d3cb2996f7b2809437cb4380da8c443263e5a7
Mateusz Garbulowski <mateuszgarbulowski@gmail.com>
2020-10-19 11:02:33 +0200

    Update plotMeanROC.R

--- R/plotMeanROC.R
+++ R/plotMeanROC.R
@@ -1,13 +1,18 @@
-plotMeanROC <- function(out){
+plotMeanROC <- function(out, col="orangered3", backCol="snow2"){
   
   ROCstats <- out$ROCstats
+  
+  if(is.null(ROCstats)){
+  ROCstats <- out$ROC.stats  
+  }
+  
   OMSpec <- rowMeans(unstack(ROCstats, form = OneMinusSpecificity ~ CVNumber))
   Sens <- rowMeans(unstack(ROCstats, form = Sensitivity ~ CVNumber))
   
   ## plotting with new colours
-  plot(OMSpec, Sens, type = "l", lwd=3, col="orangered3", xlab="1 - specificity (FPR)", ylab="sensitivity (TPR)", axes=F, cex.lab=1.5)
-  polygon(c(1,OMSpec), c(0,Sens), lwd=0.01, col="snow2")
-  text(0.75, 0.1, col="orangered3", paste0("mean AUC = ",round(out$quality$ROC.AUC.MEAN, digits = 2)),cex = 1.5)
+  plot(OMSpec, Sens, type = "l", lwd=3, col=col, xlab="1 - specificity (FPR)", ylab="sensitivity (TPR)", axes=F, cex.lab=1.5)
+  polygon(c(1,OMSpec), c(0,Sens), lwd=0.01, col=backCol)
+  text(0.75, 0.1, col=col, paste0("mean AUC = ",round(out$quality$ROC.AUC.MEAN, digits = 2)),cex = 1.5)
   axis(side=1, at=seq(0,1,0.1))
   axis(side=2, at=seq(0,1,0.1))
   
